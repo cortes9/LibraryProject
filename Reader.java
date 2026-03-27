dd10e1b6c766422a7c6e390ef234a382b9d44da8
cortes9 <lcortescedillo@csumb.edu>
2026-03-26 22:37:17 -0700

    Reader done?

--- Reader.java
+++ Reader.java
@@ -2,16 +2,17 @@ import Utilities.Code;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
- * Name: Luis Cortes Cedillo
- * Assigment: Reader.java
+ * Name: Luis Cortes
+ * Assignment: Reader.java
  */
-
 public class Reader {
+
     public static final int CARD_NUMBER_ = 0;
     public static final int NAME_ = 1;
-    public static final int PHONE = 2;
+    public static final int PHONE_ = 2;
     public static final int BOOK_COUNT_ = 3;
     public static final int BOOK_START_ = 4;
 
@@ -20,50 +21,96 @@ public class Reader {
     private String phone;
     private List<Book> books;
 
-    public Reader(int cardNumber, String name, String phone){
+    public Reader(int cardNumber, String name, String phone) {
         this.cardNumber = cardNumber;
         this.name = name;
         this.phone = phone;
         this.books = new ArrayList<>();
-
     }
 
-    public Code addBook(Book book){
-        if (hasBook(book)){
+    public Code addBook(Book book) {
+        if (hasBook(book)) {
             return Code.BOOK_ALREADY_CHECKED_OUT_ERROR;
         }
-        book.add(book);
+        books.add(book);
         return Code.SUCCESS;
-
     }
 
-    public Code removeBook(Book book){
-        if (!hasBook(book)){
-            return Code.READE_DOSENT_HAVE_BOOK_ERROR;
+    public Code removeBook(Book book) {
+        if (!hasBook(book)) {
+            return Code.READER_DOESNT_HAVE_BOOK_ERROR;
         }
-        if (books.remove(book)){
+        if (books.remove(book)) {
             return Code.SUCCESS;
         }
-        return Code.READER_COULD_NOT_REMOVE_BOOK_ERROR
+        return Code.READER_COULD_NOT_REMOVE_BOOK_ERROR;
     }
 
-    public boolean hasBook(Book book){
-        return book.contains(book);
+    public boolean hasBook(Book book) {
+        return books.contains(book);
     }
 
-    public int getBookCount(){
+    public int getBookCount() {
         return books.size();
     }
 
-    public int getBookSCount(){
-        return books.size();
-    }
-
-    public List<Book> getBooks(){
+    public List<Book> getBooks() {
         return books;
-
     }
 
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
 
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Reader)) return false;
+        Reader reader = (Reader) o;
+        return cardNumber == reader.cardNumber &&
+                Objects.equals(name, reader.name) &&
+                Objects.equals(phone, reader.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cardNumber, name, phone);
+    }
+
+    @Override
+    public String toString() {
+        String result = name + " (#" + cardNumber + ") has checked out {";
+        for (int i = 0; i < books.size(); i++) {
+            result += books.get(i);
+            if (i < books.size() - 1) {
+                result += ", ";
+            }
+        }
+        result += "}";
+        return result;
+    }
 }
-
