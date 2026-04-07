c77ffb5fbee3a72b5cff3291454f5e53d64c53a5
cortes9 <lcortescedillo@csumb.edu>
2026-04-06 21:27:57 -0700

    Shelf 3

--- Shelf.java
+++ Shelf.java
@@ -52,4 +52,67 @@ public class Shelf {
     public String toString() {
         return shelfNumber + " : " + subject;
     }
+
+    public Code addBook(Book book) {
+        if (books.containsKey(book)) {
+            books.put(book, books.get(book) + 1);
+            return Code.SUCCESS;
+        }
+
+        if (book.getSubject().equals(subject)) {
+            books.put(book, 1);
+            return Code.SUCCESS;
+        }
+
+        return Code.SHELF_SUBJECT_MISMATCH_ERROR;
+    }
+
+    public Code removeBook(Book book) {
+        if (!books.containsKey(book)) {
+            return Code.BOOK_NOT_IN_INVENTORY_ERROR;
+        }
+
+        if (books.get(book) == 0) {
+            return Code.BOOK_NOT_IN_INVENTORY_ERROR;
+        }
+
+        books.put(book, books.get(book) - 1);
+        return Code.SUCCESS;
+    }
+
+    public int getBookCount(Book book) {
+        if (books.containsKey(book)) {
+            return books.get(book);
+        }
+        return -1;
+    }
+
+    public String listBooks() {
+        String result;
+
+        if (books.size() == 1) {
+            result = "1 book on shelf: " + toString();
+        } else {
+            result = books.size() + " books on shelf: " + toString();
+        }
+
+        for (Book b : books.keySet()) {
+            result += "\n" + b + " " + books.get(b);
+        }
+
+        return result;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Shelf)) return false;
+
+        Shelf other = (Shelf) o;
+        return shelfNumber == other.shelfNumber &&
+                Objects.equals(subject, other.subject);
+    }
+
+    public int hashCode() {
+        return Objects.hash(shelfNumber, subject);
+    }
 }
