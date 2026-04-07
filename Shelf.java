6d5e081b2a976cf4f35963a9630a4b501ef19d10
cortes9 <lcortescedillo@csumb.edu>
2026-04-06 21:00:55 -0700

    Shelf

--- Shelf.java
+++ Shelf.java
@@ -1,2 +1,55 @@
+import Utilities.Code;
+import java.util.HashMap;
+import java.util.Objects;
+
+/**
+ * Name: Luis Cortes
+ * Assignment: Shelf.java
+ */
 public class Shelf {
+
+    public static final int SHELF_NUMBER_ = 0;
+    public static final int SUBJECT_ = 1;
+
+    private HashMap<Book, Integer> books;
+    private int shelfNumber;
+    private String subject;
+
+    public Shelf() {
+        books = new HashMap<>();
+    }
+
+    public Shelf(int shelfNumber, String subject) {
+        this.shelfNumber = shelfNumber;
+        this.subject = subject;
+        books = new HashMap<>();
+    }
+
+    public HashMap<Book, Integer> getBooks() {
+        return books;
+    }
+
+    public void setBooks(HashMap<Book, Integer> books) {
+        this.books = books;
+    }
+
+    public int getShelfNumber() {
+        return shelfNumber;
+    }
+
+    public void setShelfNumber(int shelfNumber) {
+        this.shelfNumber = shelfNumber;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String toString() {
+        return shelfNumber + " : " + subject;
+    }
 }
